723da816f2a4b4e761429d1d981f10e3060af69a
Poltergeist <spectator.networx@gmail.com>
2015-07-13 16:46:01 +0200

    feat(generator): add priorities table with header.

--- app/scripts/components/generator-priority-table.react.js
+++ app/scripts/components/generator-priority-table.react.js
@@ -7,7 +7,18 @@ export default class GeneratorPriorityTable extends React.Component {
 
   render() {
     return (
-      <table className="generator__priorities-table"></table>
+      <table className="generator__priorities-table">
+        <thead className="generator__priorities-header">
+          <td>Categories</td>
+          <td>A</td>
+          <td>B</td>
+          <td>C</td>
+          <td>D</td>
+          <td>E</td>
+        </thead>
+        <tr>
+        </tr>
+      </table>
     );
   }
 }
--- test/generator-priority-table-spec.js
+++ test/generator-priority-table-spec.js
@@ -14,4 +14,17 @@ describe('Generator Priority Table', () => {
     let className = component.props.className;
     assert.equal(className, 'generator__priorities-table');
   });
+
+  it('is a table element', () => {
+    let type = component.type;
+    assert.equal(type, 'table');
+  });
+
+  it('has the right header', () => {
+    let header = component.props.children[0].props.className;
+
+    assert.equal(header, 'generator__priorities-header');
+  });
+
+  
 });
